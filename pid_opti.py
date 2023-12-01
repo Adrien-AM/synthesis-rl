64f4e621034e8a184c6a89a1570efb5ef30fa5d7
Sylvain Brisset <brissetsylvain33@gmail.com>
2023-12-01 14:46:02 +0100

    Added wind

--- pid_opti.py
+++ pid_opti.py
@@ -55,7 +55,7 @@ class LunarController:
         return ACTION_NOTHING
 
 def display(Ps, Ds):
-    env = gym.make("LunarLander-v2", render_mode="human")
+    env = gym.make("LunarLander-v2", render_mode="human", wind_power=20)
     observation, info = env.reset()
     controller = LunarController(Ps=Ps, Ds=Ds)
 
@@ -75,7 +75,7 @@ def display(Ps, Ds):
     env.close()
 
 def evaluate(Ps, Ds, nb_tests = 50):
-    env = gym.make("LunarLander-v2")
+    env = gym.make("LunarLander-v2", wind_power=20)
     observation, info = env.reset()
 
     controller = LunarController(Ps=Ps, Ds=Ds)
@@ -146,6 +146,8 @@ if __name__ == "__main__":
     avg_reward = evaluate(best_params[0], best_params[1], nb_epochs)
     print(f'Average reward on {nb_epochs} iterations : {avg_reward:.2f}')
     print(f'Total time : {time.time() - t:.2f}s')
+
+    # best_params = [[ 1.06198339,  0.22837168, -1.65524936], [ 1.7604426 , 25.82791385,  0.34561309]]
 
     display(best_params[0], best_params[1])
 
