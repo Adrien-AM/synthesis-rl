341d727403a07886d1efbe36cd61bcb6f47c1bdf
AGUILA-MULTNER Adrien <aaguilamultn@bordeaux-inp.fr>
2023-12-08 15:09:46 +0100

    PID controllers solved the game

--- pid.py
+++ pid.py
@@ -1,9 +1,53 @@
 import gymnasium as gym
+import random
+import math
 
 ACTION_NOTHING = 0
 ACTION_LEFT = 1
 ACTION_MAIN = 2
 ACTION_RIGHT = 3
+
+def temperature(t):
+    return .99 * t
+
+def neighbours(params):
+    # i = random.randrange(len(params))
+    for i in range(len(params)):
+        p, d = params[i]
+        p += random.uniform(-.1, .1)
+        d += random.uniform(-.1, .1)
+        params[i] = (p, d)
+    return params
+
+def proba(e, e_new, t):
+    de = (e - e_new)
+    if de < 0:
+        return 1
+    return math.exp(-de / t)
+
+def optimize(params, n, E):
+    '''
+    params : p and d for all controllers
+    n : number of iterations
+    E : energy  function
+    '''
+    c = LunarController(params)
+    current_energy = E(c, 1000)
+    t = 10
+    for k in range(n):
+        if k % (n // 10) == 0:
+            current_energy = E(LunarController(params), 1000)
+            print(f'Simulated Annealing iteration {k} : energy = {current_energy}')
+        t = temperature(t)
+        new_params = neighbours(params)
+        c = LunarController(new_params)
+        new_energy = E(c, 50)
+        # print(f'Temperature : {t}, proba : {proba(current_energy, new_energy, t)}')
+        if proba(current_energy, new_energy, t) >= random.random():
+            params = new_params
+            current_energy = new_energy
+
+    return params
 
 class PIDController:
     def __init__(self, P, I, D, setpoint):
@@ -30,11 +74,12 @@ class PIDController:
         return output
 
 class LunarController:
-    def __init__(self):
+    def __init__(self, params):
         # Setpoint is 0 for all 3 of them since we want to get the robot to point (0,0) with angle 0
-        self.xcontroller = PIDController(P=.5, I=0, D=2, setpoint=0)
-        self.ycontroller = PIDController(P=.1, I=0, D=25, setpoint=0)
-        self.acontroller = PIDController(P=-.8, I=-0, D=0, setpoint=0)
+        (px, dx), (py, dy), (pa, da) = params
+        self.xcontroller = PIDController(P=px, I=0, D=dx, setpoint=0)
+        self.ycontroller = PIDController(P=py, I=0, D=dy, setpoint=0)
+        self.acontroller = PIDController(P=pa, I=0, D=da, setpoint=0)
 
     def play(self, obs):
         x, y, vx, vy, a, va, l1, l2 = obs
@@ -43,19 +88,18 @@ class LunarController:
         erra = self.acontroller.update(a)
 
         if abs(errx + erra) > abs(erry):
-            if (errx + erra) < -.1:
+            if (errx + erra) < -1:
                 return ACTION_LEFT
-            elif (errx + erra) > .1:
+            elif (errx + erra) > 1:
                 return ACTION_RIGHT
-        elif erry > .1:
+        elif erry > 1:
             return ACTION_MAIN
         
         return ACTION_NOTHING
 
-def display():
+def display(controller):
     env = gym.make("LunarLander-v2", render_mode="human")
     observation, info = env.reset()
-    controller = LunarController()
 
     done = False
     truncated = False
@@ -68,38 +112,36 @@ def display():
             env.reset()
             break
         env.render()
-
-    print(f'Total reward : {total_reward:.2f}')
     env.close()
 
-def evaluate(nb_epochs = 1000):
-    env = gym.make("LunarLander-v2")
+def evaluate(controller, nb_tests = 50):
+    env = gym.make("LunarLander-v2", wind_power=20)
     observation, info = env.reset()
-    controller = LunarController()
 
-    i = 0
-    s = 0
-    while i < nb_epochs:
-        if i % (nb_epochs//10) == 0:
-            print(f'Iteration {i}/{nb_epochs}')
-        i += 1
+    total_reward = 0
+    for j in range(nb_tests):
         done = False
-        truncated = False
-        total_reward = 0
-        while True:
+        while not done:
             move = controller.play(observation)
             observation, reward, terminated, truncated, info = env.step(move)
             total_reward += reward
-            if (terminated or truncated):
+            done = terminated or truncated
+            if done:
                 observation, info = env.reset()
                 break
-        s += total_reward
-    env.close()
-    return s / nb_epochs
+            
+    avg_reward = total_reward / nb_tests
+    return avg_reward
 
 
 if __name__ == "__main__":
-    # display()
+    # [(0.7738700711164033, 2.45110950874502), (0.2587640480344262, 24.860236061123114), (-1.387602756853892, 0.7242691704651993)]
+    params = [(.5, 2), (.1, 25), (-.8, 0)]
+    # params = [(0, 0), (0, 0), (0, 0)]
+    params = optimize(params, 2000, evaluate)
+    print(f'Found parameters : {params}')
+    controller = LunarController(params)
+    display(controller)
     nb_epochs = 1000
-    avg_reward = evaluate(nb_epochs)
+    avg_reward = evaluate(controller, nb_epochs)
     print(f'Average reward on {nb_epochs} iterations : {avg_reward:.2f}')
